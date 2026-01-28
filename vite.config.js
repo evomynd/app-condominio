4ef548c61adc8a26b5217f630bd83a0a09452555
Seu Nome <leandroalves.facilities@gmail.com>
2026-01-28 19:52:30 -0300

    Fix base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
-  base: process.env.GITHUB_PAGES ? '/app-condominio/' : '/',
+  base: '/app-condominio/',
   plugins: [
     react(),
     VitePWA({
